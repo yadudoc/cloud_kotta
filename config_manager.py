627fc6895faa1f2a64afcf9dbd841dd2506f464d
Ubuntu <ubuntu@ip-10-0-126-117.ec2.internal>
2015-12-03 19:58:06 +0000

    Updated config_manager

--- config_manager.py
+++ config_manager.py
@@ -17,9 +17,11 @@ import boto.dynamodb2 as ddb
 import boto.ec2
 import boto.sqs
 import boto.sns
-import boto.ses
-from bottle import app, template
+from bottle import app
 from boto.s3.connection import S3Connection
+from datetime import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 log_levels = { "DEBUG"   : logging.DEBUG,
@@ -34,21 +36,19 @@ log_levels = { "DEBUG"   : logging.DEBUG,
 def update_creds_from_metadata_server(app):
     #Todo error check for timeout errors from http access
     #TOdo error catch for json decode failure
-
-    if "keys.expiry" in app.config :
-        print "Keys expiry : ",app.config["keys.expiry"]
-        print "current     : ",time.time()
-        if app.config["keys.expiry"] < time.time():
-            logging.debug("Update creds from metadata cancelled {0} < {1}".format(
-                app.config["keys.expiry"], time.time()))
-            return False
+    
+    if "keys.expiry" in app.config and app.config["keys.expiry"] > (datetime.now() + relativedelta(hours=1)):
+        logging.debug("Update creds from metadata cancelled {0} < {1}".format(
+            app.config["keys.expiry"], 
+            datetime.now()))
+        return False
 
     URL  = app.config["metadata.credurl"]
     role = requests.get(URL).content
     URL  = URL + role
     data = requests.get(URL).json()
 
-    app.config["keys.expiry"]     = time.strptime(str(data['Expiration']), '%Y-%m-%dT%H:%M:%SZ')
+    app.config["keys.expiry"]     = datetime.strptime(str(data['Expiration']), '%Y-%m-%dT%H:%M:%SZ')
     app.config["keys.key_id"]     = str(data['AccessKeyId'])
     app.config["keys.key_secret"] = str(data['SecretAccessKey'])
     app.config["keys.key_token"]  = str(data['Token'])
@@ -66,46 +66,6 @@ def update_creds_from_metadata_server(app):
     app.config["identity"] = data
 
     return True
-
-
-def send_success_email(data, app):
-    sesconn     =  app.config['ses.conn'] 
-    job_id      =  data.get('job_id')
-    rec_email   =  data.get('user_email')
-    rec_name    =  data.get('username')
-    src_email   =  app.config['ses.email_sender']
-    url         =  app.config['server.url']
-
-    body = template('./templates/completion_email.tpl',
-                    username=rec_name,
-                    job_id=job_id,
-                    url=url)
-
-    print body
-    st = sesconn.send_email(src_email,
-                            "[JES] Your Job has completed",
-                            body,
-                            [rec_email])
-    return st
-
-def send_failure_email(data, app):
-    sesconn     =  app.config['ses.conn'] 
-    job_id      =  data.get('job_id')
-    rec_email   =  data.get('user_email')
-    rec_name    =  data.get('username')
-    src_email   =  app.config['ses.email_sender']
-
-    body = template('./templates/failure_email.tpl',
-                    username=rec_name,
-                    job_id=job_id,
-                    url=url)
-
-    print body
-    st = sesconn.send_email(src_email,
-                            "[JES] Your Job has failed",
-                            body,
-                            [rec_email])
-    return st
 
 
 def init(app):
@@ -135,11 +95,6 @@ def init(app):
                                       aws_secret_access_key=app.config['keys.key_secret'],
                                       security_token=app.config['keys.key_token'])
 
-    ses  = boto.ses.connect_to_region(app.config["identity"]['region'],
-                                      aws_access_key_id=app.config['keys.key_id'],
-                                      aws_secret_access_key=app.config['keys.key_secret'],
-                                      security_token=app.config['keys.key_token'])
-
     s3   = S3Connection(aws_access_key_id=app.config['keys.key_id'], 
                         aws_secret_access_key=app.config['keys.key_secret'], 
                         security_token=app.config['keys.key_token'])
@@ -148,13 +103,12 @@ def init(app):
                  schema=[HashKey("job_id")],
                  connection=ddb.connect_to_region(app.config['dynamodb.region'],
                                                   aws_access_key_id=app.config['keys.key_id'], 
-                                                  aws_secret_access_key=app.config['keys.key_secret'],
+                                                  aws_secret_access_key=app.config['keys.key_secret'], 
                                                   security_token=app.config['keys.key_token']))
 
     app.config["ec2.conn"]  = ec2
     app.config["sns.conn"]  = sns
     app.config["sqs.conn"]  = sqs
-    app.config["ses.conn"]  = ses
     app.config["s3.conn"]   = s3
     app.config["dyno.conn"] = dyno
 
@@ -180,11 +134,6 @@ def connect_to_dynamodb(app):
 
     app.config["dynamodb.table"] = dyno
     return app
-
-def refresh_tokens(app):
-    status = update_creds_from_metadata_server(app)
-    if status == True:
-        init(app)
 
 def load_configs(filename):
     app = bottle.default_app()
