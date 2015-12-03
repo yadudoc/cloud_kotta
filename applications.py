fa84f0bbfc801cb4f5b8c1d31a1facb9a87d6bbc
Ubuntu <ubuntu@ip-10-0-126-117.ec2.internal>
2015-12-03 22:18:30 +0000

    Updates adding email system back

--- applications.py
+++ applications.py
@@ -1,22 +1,60 @@
 #!/usr/bin/env python
 import sys
+import os
+import subprocess
+
 sys.path.append("../ncses/doc2vec/")
 
 import pipeline2 as d2v
 
-def bash_executor(jobtype, job_id, inputs, outputs):
-    return "Done"
+def generic_executor(job_id, executable, args, inputs, outputs):
+    try :
+        print "Running {0} {1}".format(executable, args)
+        std_out = open("STDOUT.txt", 'w')
+        std_err = open("STDERR.txt", 'w')
+        print [executable, args], "stdout='STDOUT.txt', stderr='STDERR.txt'"
+        pid = subprocess.Popen([executable, args], stdout=std_out, stderr=std_err)
+        pid.wait()
+        std_out.close()
+        std_err.close()
+    # Invalid value provided
+    except ValueError as e:
+        logging.error("ValueError : {0}".format(e));
+        raise
 
-def python_executor(jobtype, job_id, inputs, outputs):
-    return "Done"
+    # Failed to execute
+    except OSError as e:
+        logging.error("OSError : {0}".format(e));
+        raise
 
-def experimental(jobtype, job_id, inputs, outputs):
-    return "Done"
+    # Unknown error
+    except Exception as e:
+        logging.error("Generic Error : {0}".format(e));
+        raise
 
+    # everything is OK!
+    return True
+
+def python_executor (job_id, executable, args, inputs, outputs):
+    return True
+
+def experimental (job_id, executable, args, inputs, outputs):
+    return True
+
+
+def doc_to_vec (job_id, executable, args, inputs, outputs):
+
+    try:
+        d2v.pipeline(inputs, outputs)
+    except Exception as e:
+        logging.error("Caught exception : {0}".format(e))
+        raise
+
+    return True
 
 # Job Definitions
-JOBS = { "doc_to_vec" : d2v.pipeline,
-         "generic"    : bash_executor,
+JOBS = { "doc_to_vec" : doc_to_vec,
+         "generic"    : generic_executor,
          "python"     : python_executor,
          "experimental": experimental }
 
@@ -28,7 +66,8 @@ def test():
          {"src": "word_mat.pkl", "dest": "klab-jobs/{0}/".format(uid)},
          {"src": "mdl.pkl",      "dest": "klab-jobs/{0}/".format(uid)}]
 
-    JOBS["doc2vec"](i, o)
-
+    #JOBS["doc_to_vec"](i, o)
+    r = JOBS["generic"]("job_1001", "/home/ubuntu/task_engine/test.sh", "foobar", [], [])
+    print "Return code ", r
 
 #test()
--- templates/completion_email.tpl
+++ templates/completion_email.tpl
@@ -2,6 +2,6 @@ Hello {{username}},
 
 You are receiving this email because your Job({{job_id}}) has completed execution.
 For more information about the job please click (or go to):
-{{url}}/{{job_id}}
+{{url}}/jobs/{{job_id}}
 
  -Job Execution Service-
