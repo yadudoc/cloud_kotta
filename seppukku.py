1cbfd08a59427dc5881e66d245403503fcb2f365
root <yadudoc1729@gmail.com>
2016-02-12 23:11:27 +0000

    Adding code to kill self instances

---
+++ seppukku.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env
+
+import requests
+import time, datetime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+metadata_server="http://169.254.169.254/latest/meta-data/"
+clean_tmp_dirs = False
+
+def get_instance_starttime() :
+    try:
+        data = requests.get(metadata_server + "local-ipv4")
+        last_mod = data.headers['last-modified']
+        return datetime.strptime(last_mod, "%a, %d %b %Y %H:%M:%S %Z")
+    except e:
+        print "Caught exception : {0}".format(e)
+
+def kill_self(app, dry_run=False):
+    instance_id = "foo"
+    rc = app.config["ec2.conn"].terminate_instances(instance_ids=[instance_id], dry_run=dry_run)
+    return rc
+
+def die_at_hour_edge () :
+    # Die at 5 mins
+    start_time = get_instance_starttime()
+    current_time = datetime.now()
+    delta =  current_time - start_time
+
+    hours = delta.total_seconds() / (60*60)
+    partial_hour =  ( delta.total_seconds() - int(hours)*60*60 ) /60
+
+    print "Minutes from hour edge ", partial_hour / 60
+
+    if partial_hour > 58 :
+        print "Time to die"
+    else:
+        print "Wait till 2 mins to hour to die"
+    
+    return delta
+
+if __name__ == "__main__" :
+    import config_manager as cm
+    app = cm.load_configs("production.conf")
+
+    while True:
+        time.sleep(1)
+        die_at_hour_edge()
